0c320354f307512b43f34eec2be5f0b28bc6fb96
adamrahmi <adamrahmi@icloud.com>
2024-01-04 21:26:59 +0100

    Inital commit

---
+++ content.js
@@ -0,0 +1,21 @@
+// content.js
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === "pasteText") {
+      const activeElement = document.activeElement;
+      if (activeElement.tagName === "INPUT" || activeElement.tagName === "TEXTAREA") {
+        // Save the current scroll position
+        const scrollTop = activeElement.scrollTop;
+  
+        // Paste the text
+        activeElement.value += request.text;
+  
+        // Resize the textbox to fit the content
+        activeElement.style.height = "auto";
+        activeElement.style.height = (activeElement.scrollHeight + 2) + "px";
+  
+        // Restore the scroll position
+        activeElement.scrollTop = scrollTop;
+      }
+    }
+  });
+  
---
+++ predefined.js
@@ -0,0 +1,14 @@
+// predefined.js
+const predefinedStrings = {
+    "Not product": {
+      "Decrease severity": "Decrease severity string",
+      "Defect pushback": "Defect pushback string",
+      "Defect opened": "Defect opened string",
+      "Zombie issue (RCA)": "Zombie issue (RCA) string",
+      "Antivirus issue": "Antivirus issue string",
+    },
+    "Qlik Sense": "Qlik Sense string",
+    "Qlik Alerting": "Qlik Alerting string",
+    "NPrinting": "NPrinting string",
+  };
+  
